72566f8f14cf5ab83c015b7fde1845d4fa5f0534
balu <baluk1219@gmail.com>
2017-12-04 10:05:16 +0530

    changes

--- server.js
+++ server.js
@@ -85,7 +85,11 @@ mongo.connect(config.db)
     console.log(err);
 });
 
-
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/driverWebAppFrontEnd/dist'));
